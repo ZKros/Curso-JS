c85dfee91f6e73d7f4cacd030cdfa4efc7b2d32d
Daniel Silva <daniel.silva@qualityautomacao.com>
2022-07-18 13:52:11 -0300

    Ajustando o IF

--- aula19/index.js
+++ aula19/index.js
@@ -1,7 +1,7 @@
 const numero = Math.floor(Math.random() * 11);
 
 if (numero >= 0 || numero <= 4){
-	console.log("Seu número está entre 0-3 ou 9-10")
+	console.log("Seu número está entre 0-4")
 } else if (numero >= 5 || numero <= 8){
 	console.log("Seu número está entre 5-8");
 } else {
