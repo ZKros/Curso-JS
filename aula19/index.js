08baaeba3fbb16eccb7ddabad4caf11317970a9b
Daniel Silva <daniel.silva@qualityautomacao.com>
2022-07-18 13:56:26 -0300

    Indentação do Codigo

--- aula19/index.js
+++ aula19/index.js
@@ -1,8 +1,8 @@
 const numero = Math.floor(Math.random() * 11);
 
-if (numero >= 0 || numero <= 4){
+if (numero >= 0 || numero <= 4) {
 	console.log("Seu número está entre 0-4")
-} else if (numero >= 5 || numero <= 8){
+} else if (numero >= 5 || numero <= 8) {
 	console.log("Seu número está entre 5-8");
 } else {
 	console.log("Seu número é maior que 9")
